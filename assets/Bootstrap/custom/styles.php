8aed77b701291ee656c787fa2fd108118dd7624a
Júlia Barão <80726258+juliabarao@users.noreply.github.com>
2022-05-24 18:27:54 -0300

    Complementando detalhes

---
+++ assets/Bootstrap/custom/styles.php
@@ -0,0 +1,30 @@
+<?php
+
+header("Content-type: text/css");
+
+?>
+
+<!-- * {
+  padding: 0;
+  margin: 0;
+  border: 0;
+} -->
+
+body {
+  box-sizing: border-box;
+  font-family:system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";;
+ 
+}
+
+.grid-container {
+  display: grid;
+  margin: 100px;
+  grid-template-columns: auto auto;
+  grid-gap: 10px;
+  padding: 10px;
+}
+
+
+.teste {
+  height: 240px
+}
